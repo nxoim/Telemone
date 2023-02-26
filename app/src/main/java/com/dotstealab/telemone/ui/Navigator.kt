583829c6379216c024a03e882d2591fbd87853c2
DotsTeaLab <dotstealab@gmail.com>
2023-02-26 09:26:31 +0100

    Navigator: added transitions between screens

--- app/src/main/java/com/dotstealab/telemone/ui/Navigator.kt
+++ app/src/main/java/com/dotstealab/telemone/ui/Navigator.kt
@@ -1,7 +1,10 @@
 package com.dotstealab.telemone.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.navigation.NavHostController
 import com.dotstealab.telemone.MainViewModel
 import com.dotstealab.telemone.ui.screens.editor.EditorScreen
@@ -13,17 +16,62 @@ import com.google.accompanist.navigation.animation.composable
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigator(navController: NavHostController, vm: MainViewModel) {
-	AnimatedNavHost(
-		navController = navController,
-		startDestination = "MainScreen"
-	) {
-		composable("MainScreen") {
+	val screenWidth = LocalConfiguration.current.screenWidthDp * 5
+
+	AnimatedNavHost(navController, "MainScreen") {
+		composable(
+			"MainScreen",
+			enterTransition = {
+				when (initialState.destination.route) {
+					"EditorScreen" -> slideInHorizontally(initialOffsetX = { -screenWidth })
+					else -> null
+				}
+			},
+			exitTransition = {
+				when (targetState.destination.route) {
+					"EditorScreen" -> slideOutHorizontally(targetOffsetX = { -screenWidth })
+					else -> null
+				}
+			}
+		) {
 			MainScreen(navController, vm)
 		}
-		composable("EditorScreen") {
+
+		composable(
+			"EditorScreen",
+			enterTransition = {
+				when (initialState.destination.route) {
+					"MainScreen" -> slideInHorizontally(initialOffsetX = { screenWidth })
+					"ThemeContentsScreen" -> slideInHorizontally(initialOffsetX = { -screenWidth })
+					else -> null
+				}
+			},
+			exitTransition = {
+				when (targetState.destination.route) {
+					"ThemeContentsScreen" -> slideOutHorizontally(targetOffsetX = { -screenWidth })
+					"MainScreen" -> slideOutHorizontally(targetOffsetX = { screenWidth })
+					else -> null
+				}
+			}
+		) {
 			EditorScreen(navController, vm)
 		}
-		composable("ThemeContentsScreen") {
+
+		composable(
+			"ThemeContentsScreen",
+			enterTransition = {
+				when (initialState.destination.route) {
+					"EditorScreen" -> slideInHorizontally(initialOffsetX = { screenWidth })
+					else -> null
+				}
+			},
+			exitTransition = {
+				when (targetState.destination.route) {
+					"EditorScreen" -> slideOutHorizontally(targetOffsetX = { screenWidth })
+					else -> null
+				}
+			}
+		) {
 			ThemeContentsScreen(vm)
 		}
 	}
