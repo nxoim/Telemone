b531ef7375ba86517fa957527e65a176ee239690
DotsTeaLab <dotstealab@gmail.com>
2023-03-03 16:26:30 +0100

    moved files to appropriate locations

--- app/src/main/java/com/dotstealab/telemone/ui/screens/editor/components/SavedThemeItem.kt
+++ app/src/main/java/com/dotstealab/telemone/ui/screens/editor/components/SavedThemeItem.kt
@@ -1,4 +1,4 @@
-package com.dotstealab.telemone.ui.screens.main.components
+package com.dotstealab.telemone.ui.screens.editor.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
