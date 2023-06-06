be9ab884d687b653109bd6988bc876bb9de6f900
DotsTeaLab <dotstealab@gmail.com>
2023-06-06 20:37:39 +0200

    added missing file

---
+++ app/src/main/java/com/number869/telemone/data/AppSettings.kt
@@ -0,0 +1,10 @@
+package com.number869.telemone.data
+
+
+// access these by context.getSharedPreferences("AppPreferences",
+// Context.MODE_PRIVATE).getSomethingSomething(
+// AppSettings.Whatever.id, whateverDefaultValue)
+enum class AppSettings(val id: String) {
+	// whenever pp and tos change - bump the version number in the id
+	AgreedToPpAndTos("userAgreedToV1OnWelcomeScreen");
+}
