afc612003ba22941b9e8bc3809007d748ff22bba
DotsTeaLab <dotstealab@gmail.com>
2023-06-06 20:53:30 +0200

    MainScreen: - added a drop down menu with a link to the about section - removed the click action from the title

--- app/src/main/java/com/number869/telemone/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/number869/telemone/ui/screens/main/MainScreen.kt
@@ -1,15 +1,24 @@
 package com.number869.telemone.ui.screens.main
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -27,14 +36,8 @@ fun MainScreen(
 	vm: MainViewModel = viewModel()
 ) {
 	Column(Modifier.fillMaxSize()) {
-		CenterAlignedTopAppBar(
-			title = {
-				Text(
-					text = "TeleMone",
-					Modifier.clickable { navController.navigate(Screens.AboutScreen.route) }
-				)
-			}
-		)
+		MainScreenAppBar(navController)
+
 		Column(
 			Modifier
 				.padding(24.dp)
@@ -49,4 +52,32 @@ fun MainScreen(
 			}
 		}
 	}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainScreenAppBar(navController: NavHostController) {
+	var showMenu by remember { mutableStateOf(false) }
+
+	CenterAlignedTopAppBar(
+		title = { Text(text = "TeleMone") },
+		actions = {
+			IconButton(onClick = { showMenu = true }) {
+				Icon(Icons.Filled.MoreVert, contentDescription = "Menu")
+			}
+
+			DropdownMenu(
+				expanded = showMenu,
+				onDismissRequest = { showMenu = false }
+			) {
+				DropdownMenuItem(
+					text = { Text("About") },
+					onClick = {
+						navController.navigate(Screens.AboutScreen.route)
+						showMenu = false
+					}
+				)
+			}
+		}
+	)
 }
