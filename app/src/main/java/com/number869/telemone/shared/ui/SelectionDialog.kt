6f1f9c05373b1e0d0ba0b07960b7353250e5d843
DotsTeaLab <dotstealab@gmail.com>
2023-07-25 18:12:44 +0200

    SeletionDialog & SelectionDialogItem: - design changes

--- app/src/main/java/com/number869/telemone/shared/ui/SelectionDialog.kt
+++ app/src/main/java/com/number869/telemone/shared/ui/SelectionDialog.kt
@@ -2,8 +2,11 @@ package com.number869.telemone.shared.ui
 
 import android.content.res.Configuration
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
@@ -37,25 +41,43 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.number869.telemone.ui.theme.TelemoneTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SelectionDialog(
 	title: String,
 	contentAlpha: () -> Float = { 1f },
 	content: LazyListScope.() -> Unit
 ) {
+	val backgroundColor = if (isSystemInDarkTheme())
+		MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp)
+	else
+		MaterialTheme.colorScheme.surface
+
 	Box(
 		Modifier
+
 			.widthIn(min = 280.dp, max = 560.dp)
+
 			.padding(horizontal = 48.dp)
+			.shadow(
+				8.dp,
+				RoundedCornerShape(24.dp)
+			)
 			.clip(RoundedCornerShape(24.dp))
-			.background(MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp))
+
+			.background(backgroundColor)
+
 	) {
-		Column(Modifier.padding(24.dp).alpha(contentAlpha())) {
+		Column(
+			Modifier
+				.padding(24.dp)
+				.alpha(contentAlpha())) {
 			Text(
 				title,
 				style = MaterialTheme.typography.headlineSmall,
-				modifier = Modifier.align(Alignment.CenterHorizontally),
-				color = MaterialTheme.colorScheme.onSurfaceVariant
+				modifier = Modifier.align(Alignment.CenterHorizontally).basicMarquee(),
+				color = MaterialTheme.colorScheme.onSurface,
+				maxLines = 1
 			)
 
 			Spacer(modifier = Modifier.height(18.dp))
@@ -76,27 +98,31 @@ fun LazyListScope.SelectionDialogItem(
 	selected: Boolean
 ) {
 	item {
+		val selectedContainerColor = if (isSystemInDarkTheme())
+			MaterialTheme.colorScheme.onPrimaryContainer
+		else
+			MaterialTheme.colorScheme.primaryContainer
+
+		val selectedContentColor = if (isSystemInDarkTheme())
+			MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+		else
+			MaterialTheme.colorScheme.onPrimaryContainer
+
 		val animatedBackgroundColor by animateColorAsState(
-			if (selected)
-				MaterialTheme.colorScheme.onPrimaryContainer
-			else
-				Color.Transparent,
+			if (selected) selectedContainerColor else Color.Transparent,
 			label = ""
 		)
 
 		val animatedTextColor by animateColorAsState(
 			if (selected)
-				MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+				selectedContentColor
 			else
 				MaterialTheme.colorScheme.onSurfaceVariant,
 			label = ""
 		)
 
 		val animatedIconColor by animateColorAsState(
-			if (selected)
-				MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
-			else
-				Color.Transparent,
+			if (selected) selectedContentColor else Color.Transparent,
 			label = ""
 		)
 
