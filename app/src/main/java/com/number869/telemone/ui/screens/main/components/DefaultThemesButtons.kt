bd0c975e4f69794ec2cb33bb2de8726b043bb618
Number869 <52751853+Number869@users.noreply.github.com>
2024-02-11 02:22:25 +0100

    DefaultThemesButtons: made layout adaptive

--- app/src/main/java/com/number869/telemone/ui/screens/main/components/DefaultThemesButtons.kt
+++ app/src/main/java/com/number869/telemone/ui/screens/main/components/DefaultThemesButtons.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +17,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,13 +35,36 @@ import com.number869.telemone.ui.theme.SolarSet
 fun DefaultThemesButtons() {
 	val vm = viewModel<MainViewModel>()
 
-	Column(Modifier.fillMaxWidth()) {
-		LightThemeButton { vm.exportTheme(light = true) }
-		DarkThemeButton(
-			Modifier
-				.align(Alignment.End)
-				.padding(top = 16.dp)) {
-			vm.exportTheme(light = false)
+	val buttons = remember {
+		movableContentOf<Pair<Modifier, Modifier>> {
+			LightThemeButton(it.first) { vm.exportTheme(light = true) }
+
+			DarkThemeButton(it.second) { vm.exportTheme(light = false) }
+		}
+	}
+
+	BoxWithConstraints {
+		if (maxHeight < 420.dp) {
+			Row(
+				Modifier.fillMaxWidth(),
+				horizontalArrangement = Arrangement.SpaceAround
+			) {
+				buttons(
+					Pair(
+						Modifier,
+						Modifier
+					)
+				)
+			}
+		} else {
+			Column(Modifier.fillMaxWidth()) {
+				buttons(
+					Pair(
+						Modifier,
+						Modifier.padding(top = 16.dp).align(Alignment.End)
+					)
+				)
+			}
 		}
 	}
 }
