8565c87748b04cafeadc3a9ea20d2ffe48e1e65a
nxoim <52751853+nxoim@users.noreply.github.com>
2024-04-22 18:27:34 +0200

    SettingsManager: rewrite for scalability

--- app/src/main/java/com/number869/telemone/data/SettingsManager.kt
+++ app/src/main/java/com/number869/telemone/data/SettingsManager.kt
@@ -7,7 +7,6 @@ import com.number869.telemone.shared.utils.inject
 import com.tencent.mmkv.MMKV
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
-import io.realm.kotlin.ext.query
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import kotlinx.coroutines.CoroutineScope
@@ -18,10 +17,9 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.mongodb.kbson.ObjectId
-import javax.annotation.concurrent.Immutable
 
 fun settingsRealm(key: String, encryptionKey: ByteArray? = null) = Realm.open(
-    RealmConfiguration.Builder(setOf(RealmSetting::class)).run {
+    RealmConfiguration.Builder(settingsRealmSchemas).run {
         name(key)
         if (encryptionKey != null) encryptionKey(encryptionKey)
         build()
@@ -30,62 +28,40 @@ fun settingsRealm(key: String, encryptionKey: ByteArray? = null) = Realm.open(
 
 class SettingsManager(private val database: Realm = settingsRealm("default")) {
     private val scope = CoroutineScope(Dispatchers.IO)
+
     fun <T> set(key: String, value: T?) = scope.launch {
         database.write {
-            runCatching { delete(this.query(RealmSetting::class, "key = '$key'")) }
-            copyToRealm(
-                RealmSetting().apply {
-                    this.key = key
-                    stringValue = value as? String
-                    booleanValue = value as? Boolean
-                    intValue = value as? Int
-                    longValue = value as? Long
-                    floatValue = value as? Float
-                    doubleValue = value as? Double
-                }
-            )
+            runCatching { delete(this.query(getSettingType(value), "key = '$key'")) }
+            copyToRealm(asSettingType(key, value))
         }
     }
 
     fun <T> setBlocking(key: String, value: T?) {
         database.writeBlocking {
-            runCatching { delete(this.query(RealmSetting::class, "key = '$key'")) }
-            copyToRealm(
-                RealmSetting().apply {
-                    this.key = key
-                    stringValue = value as? String
-                    booleanValue = value as? Boolean
-                    intValue = value as? Int
-                    longValue = value as? Long
-                    floatValue = value as? Float
-                    doubleValue = value as? Double
-                }
-            )
+            runCatching { delete(this.query(getSettingType(value), "key = '$key'")) }
+            copyToRealm(asSettingType(key, value))
         }
     }
 
     fun <T> get(key: String, defaultValue: T? = null): T? {
         val realmSettingContainer = database
-            .query<RealmSetting>("key = '$key'")
+            .query(getSettingType(defaultValue),"key = '$key'")
             .first()
             .find()
 
-        val settingFromStorage = realmSettingContainer?.let { getSupportedSettingsValue(it, defaultValue) }
-
-        return when {
-            settingFromStorage != null -> settingFromStorage
-            else -> defaultValue
-        }
+        return realmSettingContainer
+            ?.let { getValueFromSettingContainer(defaultValue, it) as T }
+            ?: defaultValue
     }
 
     fun <T> getAsStateFlow(key: String, defaultValue: T) = database
-        .query<RealmSetting>("key = '$key'")
+        .query(getSettingType(defaultValue),"key = '$key'")
         .first()
         .asFlow()
         .flowOn(Dispatchers.IO)
         .map() { queried ->
             queried.obj
-                ?.let { getSupportedSettingsValue(it, defaultValue) }
+                ?.let { getValueFromSettingContainer(defaultValue, it) as T }
                 ?: defaultValue
         }
         .stateIn(
@@ -95,53 +71,124 @@ class SettingsManager(private val database: Realm = settingsRealm("default")) {
         )
 }
 
-private open class RealmSetting : RealmObject {
-    @PrimaryKey var _id = ObjectId()
-    var key: String = ""
-    var stringValue: String? = null
-    var booleanValue: Boolean? = null
-    var intValue: Int? = null
-    var longValue: Long? = null
-    var floatValue: Float? = null
-    var doubleValue: Double? = null
+private fun <T> getValueFromSettingContainer(type: T, obj: RealmObject) = when(type) {
+    is Boolean? -> (obj as BooleanSetting).value
+    is String? -> (obj as StringSetting).value
+    is Int? -> (obj as IntSetting).value
+    is Float? -> (obj as FloatSetting).value
+    is Long? -> (obj as LongSetting).value
+    is Double? -> (obj as DoubleSetting).value
+    else -> error("unsupported setting class")
+}
+private fun <T> getSettingType(type: T) = when(type) {
+    is Boolean? -> BooleanSetting::class
+    is String? -> StringSetting::class
+    is Int? -> IntSetting::class
+    is Float? -> FloatSetting::class
+    is Long? -> LongSetting::class
+    is Double? -> DoubleSetting::class
+    else -> error("unsupported setting class")
 }
 
-@Immutable
+private fun <T> asSettingType(targetKey: String, newValue: T) = when(newValue) {
+    is Boolean? -> BooleanSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    is String? -> StringSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    is Int? -> IntSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    is Float? -> FloatSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    is Long? -> LongSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    is Double? -> DoubleSetting().apply {
+        key = targetKey
+        value = newValue
+    }
+    else -> error("unsupported setting class")
+}
+
+class BooleanSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: Boolean? = null
+}
+
+class StringSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: String? = null
+}
+
+class IntSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: Int? = null
+}
+
+class FloatSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: Float? = null
+}
+
+class LongSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: Long? = null
+}
+
+class DoubleSetting : RealmObject {
+    var _id = ObjectId()
+    @PrimaryKey var key: String = ""
+    var value: Double? = null
+}
+
+val settingsRealmSchemas = setOf(
+    BooleanSetting::class,
+    StringSetting::class,
+    IntSetting::class,
+    FloatSetting::class,
+    LongSetting::class,
+    DoubleSetting::class
+)
+
+@androidx.compose.runtime.Immutable
 data class Setting<T>(
     private val key: String,
     private val defaultValue: T,
-    private val settingsManager: SettingsManager = inject()
+    private val targetSettingsManager: SettingsManager = inject()
 ) {
-    fun set(newValue: T) = settingsManager.set(key, newValue)
-    fun setBlocking(newValue: T) = settingsManager.setBlocking(key, newValue)
-    fun get() = settingsManager.get(key, defaultValue) ?: defaultValue
-    fun getAsStateFlow() = settingsManager.getAsStateFlow(key, defaultValue)
+    fun set(newValue: T) = targetSettingsManager.set(key, newValue)
+    fun setBlocking(newValue: T) = targetSettingsManager.setBlocking(key, newValue)
+    fun get() = targetSettingsManager.get(key, defaultValue) ?: defaultValue
+    fun getAsStateFlow() = targetSettingsManager.getAsStateFlow(key, defaultValue)
     @Composable
     fun asState() = getAsStateFlow().collectAsState()
 }
 
-@Immutable
+@androidx.compose.runtime.Immutable
 data class NullableSetting<T>(
     private val key: String,
     private val defaultValue: T? = null,
-    private val settingsManager: SettingsManager = inject(),
+    private val targetSettingsManager: SettingsManager = inject(),
 ) {
-    fun set(newValue: T?) = settingsManager.set(key, newValue)
-    fun setBlocking(newValue: T?) = settingsManager.setBlocking(key, newValue)
-    fun get() = settingsManager.get(key, defaultValue)
-    fun getAsStateFlow() = settingsManager.getAsStateFlow(key, defaultValue)
+    fun set(newValue: T?) = targetSettingsManager.set(key, newValue)
+    fun setBlocking(newValue: T?) = targetSettingsManager.setBlocking(key, newValue)
+    fun get() = targetSettingsManager.get(key, defaultValue)
+    fun getAsStateFlow() = targetSettingsManager.getAsStateFlow(key, defaultValue)
     @Composable
     fun asState() = getAsStateFlow().collectAsState()
-}
-
-private fun <T> getSupportedSettingsValue(realmSettingContainer: RealmSetting, value: T): T = when (value) {
-    is String? -> (realmSettingContainer.stringValue) as T
-    is Boolean? -> (realmSettingContainer.booleanValue) as T
-    is Int? -> (realmSettingContainer.intValue) as T
-    is Long? -> (realmSettingContainer.longValue) as T
-    is Float? -> (realmSettingContainer.floatValue) as T
-    is Double? -> (realmSettingContainer.doubleValue) as T
-    else -> throw IllegalArgumentException("Requested value for ${realmSettingContainer.key} is not supported")
 }
 
 fun InstanceDeclaratorScope.settingsManagerInitializer(context: Context) {
